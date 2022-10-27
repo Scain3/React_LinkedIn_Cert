ba8f36958a2cc3533532e27a401c01e499908b0e
Safiya Cain <64106105+Scain3@users.noreply.github.com>
2022-10-27 16:28:17 -0400

    create logic for dynamic checkbox

--- Ex_Files_React_js_EssT/Exercise_Files/Ch04/04_01/start/my-app/src/App.js
+++ Ex_Files_React_js_EssT/Exercise_Files/Ch04/04_01/start/my-app/src/App.js
@@ -7,24 +7,20 @@ import { useState, useEffect } from "react";
 // console.log(second);
 
 function App() {
-  const [emotion, setEmotion] = useState("happy");
-  const [secondary, setSecondary] = useState("tired");
+  const [checked, setChecked] = useState(false);
 
-  useEffect(()=>{
-    console.log(`It's ${emotion} right now`);
-  }, [emotion, secondary])
 
-  useEffect(() => {
-    console.log(`It's ${secondary} around here!`);
-  }, [secondary])
 
   return (
    <div className="App">
-    <h1>Current emotion is {emotion}</h1>
-    <button onClick={() => setEmotion("sad")}>Sad</button>
-    <button onClick={() => setEmotion("excited")}>Excited</button>
-    <h2>Current seconday emotion is {secondary}.</h2>
-    <button onClick={()=> setSecondary("grateful")}>Grateful</button>
+    <input
+      type="checkbox"
+      value={checked}
+      onChange={() =>
+        setChecked((checked) => !checked)
+      }
+    />
+    <label>{checked ? "checked" : "not checked"}</label>
    </div>
   );
 }
